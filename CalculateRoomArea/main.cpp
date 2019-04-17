8a72262f952d7c161fd49c21a2c7927fc2a62ecf
Kamran Mackey <kamranm1200@gmail.com>
2019-04-17 15:16:37 -0600

    Minor code rearranging.
    Signed-off-by: Kamran Mackey <kamranm1200@gmail.com>

--- CalculateRoomArea/main.cpp
+++ CalculateRoomArea/main.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 
 int main() {
-	auto room_width { 0 };
 	std::cout << "Enter the width of the room: ";
+	auto room_width{0};
 	std::cin >> room_width;
 
-	auto room_length { 0 };
 	std::cout << "Enter the length of the room: ";
+	auto room_length{0};
 	std::cin >> room_length;
 
-	const auto room_area { room_width * room_length };
+	const auto room_area{room_width * room_length};
 	std::cout << "The area of the room is " << room_area << " square feet.";
 
 	return 0;
