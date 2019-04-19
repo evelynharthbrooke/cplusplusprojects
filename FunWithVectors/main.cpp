f11d1df6e7748d0bd01b2575c03b1e362425852e
Kamran Mackey <kamranm1200@gmail.com>
2019-04-18 20:30:01 -0600

    Add FunWithVectors project.
    Signed-off-by: Kamran Mackey <kamranm1200@gmail.com>

---
+++ FunWithVectors/main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+
+// this is not generally recommended, but because of the fact that
+// not doing this leads to a little bit of ugly code and duplication,
+// use it for this program only to cleanup the code and prevent having
+// duplication.
+using namespace std;
+
+int main() {
+
+	vector<int> vector1;
+	vector<int> vector2;
+
+	vector1.push_back(10);
+	vector1.push_back(20);
+	cout << "First element in vector1: " << vector1.at(0) << "\n";
+	cout << "Second element in vector1: " << vector1.at(1) << "\n";
+	cout << "There are " << vector1.size() << " elements in vector1.\n\n";
+	vector2.push_back(100);
+	vector2.push_back(200);
+	cout << "First element in vector2: " << vector2.at(0) << "\n";
+	cout << "Second element in vector2: " << vector2.at(1) << "\n";
+	cout << "There are " << vector2.size() << " elements in vector2.\n\n";
+
+	vector<vector<int>> vector_2d;
+	vector_2d.push_back(vector1);
+	vector_2d.push_back(vector2);
+	cout << "Elements in vector_2d:" << "\n";
+	cout << vector_2d.at(0).at(0) << " " << vector_2d.at(0).at(1) << "\n";
+	cout << vector_2d.at(1).at(0) << " " << vector_2d.at(1).at(1) << "\n\n";
+
+	cout << "Changing first element in vector1 to 1000..." << "\n\n";
+	vector1.at(0) = 1000;
+
+	cout << "Elements in vector_2d: " << "\n";
+	// the multi-dimensional vector will not reflect the
+	// updated 1000 value that was assigned to the first
+	// element in vector1.
+	cout << vector_2d.at(0).at(0) << " " << vector_2d.at(0).at(1) << "\n";
+	cout << vector_2d.at(1).at(0) << " " << vector_2d.at(1).at(1) << "\n\n";
+
+	cout << "Elements in vector1: " << "\n";
+	cout << vector1.at(0) << "\n";
+	cout << vector1.at(1) << "\n";
+}
